6976491e33f8e0068bfbd48a76d6798c6ca6ab4f
Sri Harsha Grandhi <shg501378@gmail.com>
2017-09-24 00:23:13 +0530

    Signed-off-by: Sri Harsha Grandhi <shg501378@gmail.com>

--- public/script.js
+++ public/script.js
@@ -1,21 +1,30 @@
+var previousPhoto1 = document.createElement("img") ;
+previousPhoto1.src = "screenshots/2014A1PS356H";
+var previousPhoto2 = document.createElement("img") ;
+previousPhoto2.src = "screenshots/2014A1PS508H";
 $(document).ready(function () {
     var data;
-
     $.post("http://localhost:8080/data/", function (dataa) {
         data = dataa;
     });
 
     $("#onee").click(function () {
-        var rnd = Math.floor(Math.random() * data.length);
-        if (rnd === 0) {
-            rnd = 1;
-        }
         do{
-        var rnd2 = Math.floor(Math.random() * data.length)+1;
+            var rnd = Math.floor(Math.random() * data.length);
+            if (rnd === 0) {
+                rnd = 1;
+            }
+        }while(previousPhoto1==data[rnd])
+        do{
+            do{
+            var rnd2 = Math.floor(Math.random() * data.length)+1;
             if (rnd2 === 0) {
                 rnd2 = 1;
             }
         }while(rnd==rnd2)
+    }while(previousPhoto2==data[rnd2])
+    previousPhoto1 = data[rnd];
+    previousPhoto2 = data[rnd2];
         console.log(data[rnd]);
         console.log(data[rnd2]);
         var htmlCode = '<img id="one" class=\"who\" src=\"' + data[rnd] + '\" />';
@@ -24,16 +33,22 @@ $(document).ready(function () {
         $('#twoo').html(htmlCode2);
     });
     $("#twoo").click(function () {
-        var rnd = Math.floor(Math.random() * data.length);
-        if (rnd === 0) {
-            rnd = 1;
-        }
         do{
-        var rnd2 = Math.floor(Math.random() * data.length)+1;
+            var rnd = Math.floor(Math.random() * data.length);
+            if (rnd === 0) {
+                rnd = 1;
+            }
+        }while(previousPhoto1==data[rnd])
+        do{
+            do{
+            var rnd2 = Math.floor(Math.random() * data.length)+1;
             if (rnd2 === 0) {
                 rnd2 = 1;
             }
         }while(rnd==rnd2)
+    }while(previousPhoto2==data[rnd2])
+    previousPhoto1 = data[rnd];
+    previousPhoto2 = data[rnd2];
         console.log(data[rnd]);
         console.log(data[rnd2]);
         var htmlCode = '<img id="one" class=\"who\" src=\"' + data[rnd] + '\" />';
@@ -42,3 +57,5 @@ $(document).ready(function () {
         $('#twoo').html(htmlCode2);
     })
 })
+previousPhoto1 = rnd;
+previousPhoto2 = rnd2;
